b5bc0fac8c912a5afa489f3465cb05f9e4b0f12f
henriqwe <henriqwe96@gmail.com>
2022-01-06 16:48:15 -0300

    feat: adicionado atualizacao de positcao e eventos de mouse do marker e atualizacao de cor e rotacao do icone

--- src/pages/erp/monitoramento/localizacao.tsx
+++ src/pages/erp/monitoramento/localizacao.tsx
@@ -70,6 +70,10 @@ export function Page() {
   let line: google.maps.Polyline
   let marker: google.maps.Marker
   let timer: timerProps
+  const allMarkerVehiclesStep: google.maps.Marker[] = []
+  const [allMarkerVehicles, setAllMarkerVehicles] = useState<
+    google.maps.Marker[]
+  >([])
   const [mapa, setMapa] = useState<google.maps.Map>()
 
   function initMap() {
@@ -197,9 +201,19 @@ export function Page() {
       ?.filter((vehicle) => {
         if (vehicle.latitude && vehicle.longitude) return vehicle
       })
-      .map((vehicle) => {
-        createNewVehicleMarker(mapa, vehicle)
+      .forEach((vehicle) => {
+        const marker = allMarkerVehicles.find((elem) => {
+          if (elem.id === vehicle.carro_id) return elem
+        })
+        if (marker) {
+          updateVehicleMarker(marker, vehicle, mapa)
+          return
+        }
+        if (vehicle.carro_id) {
+          createNewVehicleMarker(mapa, vehicle, allMarkerVehiclesStep)
+        }
       })
+    setAllMarkerVehicles([...allMarkerVehicles, ...allMarkerVehiclesStep])
   }, [allUserVehicle])
 
   useEffect(() => {
@@ -227,11 +241,13 @@ export function Page() {
 
 function createNewVehicleMarker(
   map: google.maps.Map | undefined,
-  vehicle: vehicle
+  vehicle: vehicle,
+  allMarkerVehiclesStep: google.maps.Marker[] | any[]
 ) {
   const marker = new google.maps.Marker({
     map,
     zIndex: 2,
+    id: vehicle.carro_id,
     position: { lat: Number(vehicle.latitude), lng: Number(vehicle.longitude) },
     icon: {
       path: 'M17.402,0H5.643C2.526,0,0,3.467,0,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644 V6.584C23.044,3.467,20.518,0,17.402,0z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z',
@@ -243,7 +259,6 @@ function createNewVehicleMarker(
       rotation: Number(vehicle.crs)
     }
   })
-
   const infowindow = new google.maps.InfoWindow({
     content: `<div class='text-dark-7'>
     <p><b>Última atualização:</b> ${vehicle.date_rastreador}</p>
@@ -262,6 +277,7 @@ function createNewVehicleMarker(
   marker.addListener('mouseout', () => {
     infowindow.close()
   })
+  allMarkerVehiclesStep.push(marker)
 }
 
 function setVehicleColor(vehicle: vehicle) {
@@ -303,4 +319,44 @@ function moveMarker(
     icon.rotation = heading
     markers.setIcon(icon)
   }
+}
+
+function updateVehicleMarker(
+  marker: google.maps.Marker,
+  vehicle: vehicle,
+  map: google.maps.Map
+) {
+  const currentMarkerPos = new google.maps.LatLng(
+    Number(vehicle.latitude),
+    Number(vehicle.longitude)
+  )
+  marker.setPosition(currentMarkerPos)
+
+  const icon = marker.getIcon()
+  icon!.fillColor = setVehicleColor(vehicle)
+  icon!.rotation = Number(vehicle.crs)
+  marker.setIcon(icon)
+
+  const infowindow = new google.maps.InfoWindow({
+    content: `<div class='text-dark-7'>
+    <p><b>Última atualização:</b> ${vehicle.date_rastreador}</p>
+    <p><b>Placa:</b> ${vehicle.placa}</p>
+    <p><b>Velocidade:</b> ${Math.floor(Number(vehicle.speed))} Km/H</p>
+    <p><b>Ignição:</b> ${vehicle.ligado ? 'Ligado' : 'Desligado'}</p>
+    </div> `
+  })
+
+  google.maps.event.clearListeners(marker, 'mouseover')
+  google.maps.event.clearListeners(marker, 'mouseout')
+
+  marker.addListener('mouseover', () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false
+    })
+  })
+  marker.addListener('mouseout', () => {
+    infowindow.close()
+  })
 }
